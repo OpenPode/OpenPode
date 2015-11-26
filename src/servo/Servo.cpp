a0e52575063174bc1a1f90f0378b2f00ae3c0cb4
juliencombattelli <julien.combattelli@hotmail.com>
2015-11-26 21:05:20 +0100

    Recalibrate servomotors

--- src/servo/Servo.cpp
+++ src/servo/Servo.cpp
@@ -12,10 +12,10 @@ static const int offset_table[2][3][3] =
 		{// left
 				{345, 275, 255}, // front
 				{385, 335, 290}, // middle
-				{345, 280, 360}  // back
+				{345, 255, 360}  // back
 		},
 		{// right
-				{285, 365, 410}, // front
+				{320, 365, 410}, // front
 				{280, 340, 355}, // middle
 				{315, 345, 300}  // back
 		}     // tib  fem  cox
